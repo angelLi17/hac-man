cfd49b1e979201f9ee902c3ed7d03a8d6ada3870
Neya <neya@tmps-Mac-mini-2.local>
2024-11-23 11:49:28 -0800

    Empty Xcode project

---
+++ hac-man/hac-man/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  hac-man
+//
+//  Created by Neya on 11/23/24.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
