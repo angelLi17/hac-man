0c9d169d90ec18b3cec7cb6fc4ea9e9a7b2dc746
jsguan2452 <jsguan2452@gmail.com>
2024-11-24 10:23:01 -0800

    animations for pacman

--- hac-man/hac-man/GameScene/GameScene.swift
+++ hac-man/hac-man/GameScene/GameScene.swift
@@ -10,20 +10,33 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var pacman = SKSpriteNode(imageNamed: "pacman1")
-    private var playerXVelocity: CGFloat = 0
-    private var playerYVelocity: CGFloat = 0
-    private var playerSpeed = 3.0
-
+    var pacman = Player(xVelocity: 0,yVelocity: 1,playerSpeed: 2.0)
+    var rightAnimation = SKAction()
+    var leftAnimation = SKAction()
+    var upAnimation = SKAction()
+    var downAnimation = SKAction()
     
     override func didMove(to view: SKView) {
+        rightAnimation = SKAction.animate(with: pacman.rightWalkTextures, timePerFrame: 0.2)
+        leftAnimation = SKAction.animate(with: pacman.leftWalkTextures, timePerFrame: 0.2)
+        upAnimation = SKAction.animate(with: pacman.upWalkTextures, timePerFrame: 0.2)
+        downAnimation = SKAction.animate(with: pacman.downWalkTextures, timePerFrame: 0.2)
         backgroundColor = SKColor.black
         pacman.position = CGPoint(x: size.width / 2, y: size.height / 2)
-        pacman.texture?.filteringMode = .nearest
-        pacman.setScale(5)
+        pacman.setScale(0.5)
         pacman.physicsBody = SKPhysicsBody(circleOfRadius:8)
         pacman.physicsBody?.isDynamic = false
         addChild(pacman)
+        
+        if pacman.xVelocity == -1 {
+            pacman.run(SKAction.repeatForever(leftAnimation))
+        } else if pacman.xVelocity == 1 {
+            pacman.run(SKAction.repeatForever(rightAnimation))
+        } else if pacman.yVelocity == -1 {
+            pacman.run(SKAction.repeatForever(downAnimation))
+        } else if pacman.yVelocity == 1 {
+            pacman.run(SKAction.repeatForever(upAnimation))
+        }
     }
     
     
@@ -54,17 +67,21 @@ class GameScene: SKScene {
     override func keyDown(with event: NSEvent) {
         switch event.keyCode {
         case 123:
-            playerXVelocity = -1
-            playerYVelocity = 0
+            pacman.run(SKAction.repeatForever(leftAnimation))
+            pacman.xVelocity = -1
+            pacman.yVelocity = 0
         case 124:
-            playerXVelocity = 1
-            playerYVelocity = 0
+            pacman.run(SKAction.repeatForever(rightAnimation))
+            pacman.xVelocity = 1
+            pacman.yVelocity = 0
         case 125:
-            playerYVelocity = -1
-            playerXVelocity = 0
+            pacman.run(SKAction.repeatForever(downAnimation))
+            pacman.xVelocity = 0
+            pacman.yVelocity = -1
         case 126:
-            playerYVelocity = 1
-            playerXVelocity = 0
+            pacman.run(SKAction.repeatForever(upAnimation))
+            pacman.xVelocity = 0
+            pacman.yVelocity = 1
         case 0x31:
             print("case 0x31")
         default:
@@ -74,8 +91,8 @@ class GameScene: SKScene {
     
     
     override func update(_ currentTime: TimeInterval) {
-        pacman.position.x += playerXVelocity * playerSpeed
-        pacman.position.y += playerYVelocity * playerSpeed
+        pacman.position.x += pacman.xVelocity * pacman.playerSpeed
+        pacman.position.y += pacman.yVelocity * pacman.playerSpeed
     }
 
     func dots() {
