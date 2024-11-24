832cbd9807553d33105f48c66683cc4fb2a07742
jsguan2452 <jsguan2452@gmail.com>
2024-11-24 10:40:23 -0800

    player

--- hac-man/hac-manCharacters/Ghost.swift
+++ hac-man/hac-manCharacters/Ghost.swift
@@ -5,3 +5,12 @@
 //  Created by Janet Guan on 11/23/24.
 //
 
+class Ghost {
+    
+//    var frightened : Bool
+//    var xVelocity: Double
+//    var yVelocity: Double
+//    var speed: Double
+    
+    
+}
